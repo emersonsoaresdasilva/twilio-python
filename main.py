aba3a4dbdb8ad4e37886da45e64f6f2744be2d20
emersonsoaresdasilva <emerson.soares@aluno.faculdadeimpacta.com.br>
2022-03-27 15:22:53 -0300

    Altera parametros para envio de mensagem

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from module.whatsapp import envia_mensagem_whatsapp
 
 # Cria uma função para enviar SMS e WhatsApp
 def envia_mensagem(numero, mensagem):
-    # Envia SMS ao destinatário
-    envia_mensagem_sms(numero, mensagem)
-    # Envia WhatsApp ao destinatário
-    envia_mensagem_whatsapp(numero, mensagem)
+    # Envia SMS e WhatsApp ao destinatário
+    return envia_mensagem_sms(numero, mensagem), envia_mensagem_whatsapp(numero, mensagem)
 
 if __name__ == '__main__':
     # Carrega as variáveis de ambiente
     load_dotenv()
     # Envia SMS para o destinatário
-    print(envia_mensagem_sms('', ''))
+    print(envia_mensagem_sms(numero='', mensagem=''))
     # Envia WhatsApp para o destinatário
-    print(envia_mensagem_whatsapp('', ''))
+    print(envia_mensagem_whatsapp(numero='', mensagem=''))
     # Envia SMS e WhatsApp para o destinatário
-    print(envia_mensagem('', ''))
+    print(envia_mensagem(numero='', mensagem=''))
