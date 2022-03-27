92d4b6ff0d6cef378c1559e5f83761f5ff166848
emersonsoaresdasilva <emerson.soares@aluno.faculdadeimpacta.com.br>
2022-03-26 21:48:56 -0300

    Cria funcao para envio de SMS

---
+++ module/sms.py
@@ -0,0 +1,23 @@
+import os
+from twilio.rest import Client
+
+# Cria uma função para enviar SMS
+def envia_mensagem_sms(numero, mensagem):
+    # Busca a chave da conta no arquivo .env
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    # Busca o token da conta no arquivo .env
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    # Cria um objeto cliente do Twilio
+    client = Client(account_sid, auth_token)
+    try:
+        # Tenta enviar a mensagem
+        client.messages.create(
+                            messaging_service_sid=os.environ.get('TWILIO_MESSAGING_SERVICE_SID'),
+                            body=mensagem,
+                            to=f'+55{numero}'
+        )
+        # Caso tenha enviado, retorna a mensagem
+        return f'\nSMS enviado com sucesso, para o número: {numero}'
+    except Exception:
+        # Caso não tenha enviado, retorna o erro
+        return f'\nNão foi possível enviar o SMS, verifique o número digitado: {numero}'
